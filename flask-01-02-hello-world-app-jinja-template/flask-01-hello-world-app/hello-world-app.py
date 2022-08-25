90bf31f533ee1963068f16cfc2f909e6eb32e5da
ozkan <ozkanyuce01@gmail.com>
2022-08-25 11:34:30 +0200

    flask 01-02 created

---
+++ flask-01-02-hello-world-app-jinja-template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return "<h1>Hello World Özkan This is from Clarusway DE<h1>"
+
+
+@app.route('/second')
+def second():
+    return "This is the second page"
+
+@app.route('/third/subthird')
+def third():
+    return "This is the subpage of third page"
+
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id of this page is {id}'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
